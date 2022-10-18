0d4f80331387fcdcc13cb40b9091fd9532b5f569
nurulcse7 <nurul.cse7@gmail.com>
2022-10-18 21:25:29 +0600

    md updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+// const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
